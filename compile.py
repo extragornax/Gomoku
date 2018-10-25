0f04e37091c634fed43f8f3330b1c2138a709244
Arthur Philippe <arthur.philippe@epitech.eu>
2018-10-25 09:23:05 +0200

    started #1

---
+++ compile.py
@@ -0,0 +1,11 @@
+import os
+
+import subprocess, sys
+
+p = subprocess.Popen(["powershell.exe",
+               '-ExecutionPolicy',
+                             'Unrestricted',".\\install-go.ps1", "-version", "1.11.1"],
+              stdout=sys.stdout)
+p.communicate()
+# os.system("powershell install-go.ps1 -version 1.11.1")
+# os.system("cp gomoku ")
