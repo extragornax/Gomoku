a1ec7367215793a1662eb29cac20849fa2b282e2
Arthur Philippe <arthur.philippe@epitech.eu>
2018-10-25 14:27:12 +0200

    completed installation script

--- compile.py
+++ compile.py
@@ -4,7 +4,7 @@ import subprocess, sys
 
 p = subprocess.Popen(["powershell.exe",
                '-ExecutionPolicy',
-                             'Unrestricted',".\\install-go.ps1", "-version", "1.11.1"],
+                'Unrestricted',".\\install-gomoku.ps1"],
               stdout=sys.stdout)
 p.communicate()
 # os.system("powershell install-go.ps1 -version 1.11.1")
--- install-gomoku.ps1
+++ install-gomoku.ps1
@@ -3,24 +3,24 @@ if (-not (Test-Path Env:\GOROOT)) {
 
 Go is not installed.
 To install go on this machine,
-run the install-go.ps1 script provided in this repo.
+run as administrator the install-go.ps1 script provided in this repo.
 "@
 	exit 1
 }
 
 $directory = Get-Location
+if ("$directory" -ne "$env:GOPATH\src\gomoku") {
+    if (-not (Test-Path ${Env:GOPATH})) {
+        mkdir "$directory\go" -Force
+        $env:GOPATH = "$directory\go"
+    }
+    mkdir "$env:GOPATH\src\gomoku\gomoku" -Force
+    mkdir "$env:GOPATH\src\gomoku\pepito" -Force
+    echo "$env:GOPATH"
 
-${Env:\GOPATH} = ""
-
-if (-not (Test-Path Env:\GOPATH)) {
-	mkdir "$directory\go" -Force
-	$env:GOPATH = "$directory\go"
+    Copy-Item .\gomoku\*.go "$env:GOPATH\src\gomoku\gomoku"
+    Copy-Item .\pepito\*.go "$env:GOPATH\src\gomoku\pepito"
 }
-mkdir "$env:GOPATH\src\gomoku\gomoku" -Force
-mkdir "$env:GOPATH\src\gomoku\pepito"  -Force
-echo "$env:GOPATH"
-
-Copy-Item .\gomoku\*.go "$env:GOPATH\src\gomoku\gomoku"
-Copy-Item .\pepito\*.go "$env:GOPATH\src\gomoku\pepito"
 
 go build gomoku/pepito
+Move-Item .\pepito.exe "pbrain-PARIS-Witrand.Gaspard.exe" -Force
