300fef13b2672a925f3504ec2c9074c16315953a
Florence HU <florence.hu@epitech.eu>
2018-10-25 11:06:03 +0200

    [#3] added horizontal & vertical search functions

--- gomoku/Board_test.go
+++ gomoku/Board_test.go
@@ -1,6 +1,8 @@
 package gomoku
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestBoardCreateOK(t *testing.T) {
 	var board Board
@@ -38,5 +40,57 @@ func TestBoardCreateKO(t *testing.T) {
 	err = board.init(0)
 	if err == nil || err.Error() != ErrBoardTooSmall {
 		t.Error("Expected err board too small")
+	}
+}
+
+func TestSearch_hor(t *testing.T) {
+	var board Board
+	var coord Coord
+	err := board.init(10)
+	if err != nil {
+		t.Error("error was supposed to be nil")
+	}
+	board.Cells[1][1] = BoardCellFoe
+	board.Cells[1][0] = BoardCellFoe
+	board.Cells[1][2] = BoardCellOwn
+	board.Cells[1][4] = BoardCellOwn
+	board.Cells[1][3] = BoardCellOwn
+	board.Cells[2][1] = BoardCellOwn
+	board.Cells[2][2] = BoardCellOwn
+	board.Cells[3][1] = BoardCellOwn
+
+	coord = board.searchHor(BoardCellOwn)
+	if coord.NbPiece != 3 || coord.CoordX != 2 || coord.CoordY != 1 {
+		t.Error("error coord hor")
+	}
+	coord = board.searchHor(BoardCellFoe)
+	if coord.NbPiece != 2 || coord.CoordX != 0 || coord.CoordY != 1 {
+		t.Error("error coord hor")
+	}
+}
+
+func TestSearch_ver(t *testing.T) {
+	var board Board
+	var coord Coord
+	err := board.init(10)
+	if err != nil {
+		t.Error("error was supposed to be nil")
+	}
+	board.Cells[2][0] = BoardCellOwn
+	board.Cells[1][1] = BoardCellFoe
+	board.Cells[3][2] = BoardCellFoe
+	board.Cells[1][2] = BoardCellOwn
+	board.Cells[2][2] = BoardCellOwn
+	board.Cells[3][2] = BoardCellOwn
+	board.Cells[2][3] = BoardCellFoe
+	board.Cells[3][3] = BoardCellFoe
+
+	coord = board.searchVer(BoardCellOwn)
+	if coord.NbPiece != 3 || coord.CoordX != 2 || coord.CoordY != 1 {
+		t.Error("error coord hor")
+	}
+	coord = board.searchVer(BoardCellFoe)
+	if coord.NbPiece != 2 || coord.CoordX != 3 || coord.CoordY != 2 {
+		t.Error("error coord hor")
 	}
 }
