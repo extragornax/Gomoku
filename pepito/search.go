f84944ce051ec062573dd6955e2bd510228c5b24
Arthur Philippe <arthur.philippe@epitech.eu>
2018-10-26 12:17:48 +0200

    Adding blockades to result

--- pepito/search.go
+++ pepito/search.go
@@ -1,6 +1,8 @@
 package main
 
-import "gomoku/gomoku"
+import (
+	"gomoku/gomoku"
+)
 
 const (
 	searchHorizontalResult = iota
@@ -11,57 +13,74 @@ const (
 
 // searchResult
 type searchResult struct {
-	resultType int
-	size       uint
-	x          uint
-	y          uint
+	resultType    int
+	size          uint
+	x             uint
+	y             uint
+	blockedBefore bool
+	blockedAfter  bool
 }
 
-func searchHorizontal(brd *gomoku.Board, player uint8) searchResult {
+func searchBlockedHorizontal(brd gomoku.Board, x uint, y uint, size uint) (before bool, after bool) {
+	before = x == 0 || brd.Cells[y][x-1] != uint8(0)
+	after = x+size+1 == brd.Size || brd.Cells[y][x+size] != uint8(0)
+	return
+	// return !(x == 0 || brd.Cells[y][x-1] != 0) ||
+	// 	(x+size+1 == brd.Size || brd.Cells[y][x+size+1] != 0)
+}
+
+func searchBlockedVertical(brd gomoku.Board, x uint, y uint, size uint) (before bool, after bool) {
+	before = y == 0 || brd.Cells[y-1][x] != 0
+	after = y+size == brd.Size || brd.Cells[y+size][x] != 0
+	return
+}
+
+func searchHorizontal(brd gomoku.Board, target uint8) searchResult {
 	var res searchResult
 	res.resultType = searchHorizontalResult
 	res.size = 0
 	res.x = 0
 	res.y = 0
-	piece := uint(0)
 
-	for i := uint(0); i < brd.Size; i++ {
-		for j := uint(0); j < brd.Size; j++ {
-			for brd.Cells[i][j] == player {
-				piece++
-				j++
+	for y := uint(0); y < brd.Size; y++ {
+		for x := uint(0); x < brd.Size; x++ {
+			size := uint(0)
+			for brd.Cells[y][x] == target {
+				size++
+				x++
 			}
-			if piece > res.size {
-				res.size = piece
-				res.y = i
-				res.x = j - piece
+			before, after := searchBlockedHorizontal(brd, x-size, y, size)
+			if size > res.size && !(before && after) {
+				res.size = size
+				res.y = y
+				res.x = x - size
 			}
-			piece = 0
 		}
 	}
 	return res
 }
 
-func searchVertical(brd *gomoku.Board, player uint8) searchResult {
+func searchVertical(brd gomoku.Board, player uint8) searchResult {
 	var res searchResult
 	res.resultType = searchVecticalResult
 	res.size = 0
 	res.x = 0
 	res.y = 0
-	piece := uint(0)
+	size := uint(0)
 
-	for j := uint(0); j < brd.Size; j++ {
-		for i := uint(0); i < brd.Size; i++ {
-			for brd.Cells[i][j] == player {
-				piece++
-				i++
+	for x := uint(0); x < brd.Size; x++ {
+		for y := uint(0); y < brd.Size; y++ {
+			for brd.Cells[y][x] == player {
+				size++
+				y++
 			}
-			if piece > res.size {
-				res.size = piece
-				res.y = i - piece
-				res.x = j
+			before, after := searchBlockedVertical(brd, x, y-size, size)
+			if size > res.size && !(before && after) {
+				res.size = size
+				res.y = y - size
+				res.x = x
 			}
-			piece = 0
+			size = 0
 		}
 	}
 	return res
--- pepito/search_test.go
+++ pepito/search_test.go
@@ -14,6 +14,11 @@ func TestSearchHorizontal(t *testing.T) {
 	}
 	board.Cells[1][1] = gomoku.BoardCellFoe
 	board.Cells[1][0] = gomoku.BoardCellFoe
+
+	board.Cells[4][2] = gomoku.BoardCellFoe
+	board.Cells[4][1] = gomoku.BoardCellFoe
+	board.Cells[4][3] = gomoku.BoardCellOwn
+
 	board.Cells[1][2] = gomoku.BoardCellOwn
 	board.Cells[1][4] = gomoku.BoardCellOwn
 	board.Cells[1][3] = gomoku.BoardCellOwn
@@ -21,12 +26,12 @@ func TestSearchHorizontal(t *testing.T) {
 	board.Cells[2][2] = gomoku.BoardCellOwn
 	board.Cells[3][1] = gomoku.BoardCellOwn
 
-	coord = searchHorizontal(&board, gomoku.BoardCellOwn)
+	coord = searchHorizontal(board, gomoku.BoardCellOwn)
 	if coord.size != 3 || coord.x != 2 || coord.y != 1 {
 		t.Error("error coord hor")
 	}
-	coord = searchHorizontal(&board, gomoku.BoardCellFoe)
-	if coord.size != 2 || coord.x != 0 || coord.y != 1 {
+	coord = searchHorizontal(board, gomoku.BoardCellFoe)
+	if coord.size != 2 || coord.x != 1 || coord.y != 4 {
 		t.Error("error coord hor")
 	}
 }
@@ -39,19 +44,55 @@ func TestSearchVertical(t *testing.T) {
 		t.Error("error was supposed to be nil")
 	}
 	board.Cells[2][0] = gomoku.BoardCellOwn
+
 	board.Cells[1][1] = gomoku.BoardCellFoe
 	board.Cells[3][2] = gomoku.BoardCellFoe
+
+	board.Cells[0][2] = gomoku.BoardCellFoe
 	board.Cells[1][2] = gomoku.BoardCellOwn
 	board.Cells[2][2] = gomoku.BoardCellOwn
 	board.Cells[3][2] = gomoku.BoardCellOwn
+	// board.Cells[4][2] = gomoku.BoardCellFoe
+
 	board.Cells[2][3] = gomoku.BoardCellFoe
 	board.Cells[3][3] = gomoku.BoardCellFoe
 
-	coord = searchVertical(&board, gomoku.BoardCellOwn)
+	coord = searchVertical(board, gomoku.BoardCellOwn)
 	if coord.size != 3 || coord.x != 2 || coord.y != 1 {
 		t.Error("error coord hor")
 	}
-	coord = searchVertical(&board, gomoku.BoardCellFoe)
+	coord = searchVertical(board, gomoku.BoardCellFoe)
+	if coord.size != 2 || coord.x != 3 || coord.y != 2 {
+		t.Error("error coord hor")
+	}
+}
+
+func TestSearchVerticalBlocked(t *testing.T) {
+	var board gomoku.Board
+	var coord searchResult
+	err := board.Init(10)
+	if err != nil {
+		t.Error("error was supposed to be nil")
+	}
+	board.Cells[2][0] = gomoku.BoardCellOwn
+
+	board.Cells[1][1] = gomoku.BoardCellFoe
+	board.Cells[3][2] = gomoku.BoardCellFoe
+
+	board.Cells[0][2] = gomoku.BoardCellFoe
+	board.Cells[1][2] = gomoku.BoardCellOwn
+	board.Cells[2][2] = gomoku.BoardCellOwn
+	board.Cells[3][2] = gomoku.BoardCellOwn
+	board.Cells[4][2] = gomoku.BoardCellFoe
+
+	board.Cells[2][3] = gomoku.BoardCellFoe
+	board.Cells[3][3] = gomoku.BoardCellFoe
+
+	coord = searchVertical(board, gomoku.BoardCellOwn)
+	if coord.size != 1 || coord.x != 0 || coord.y != 2 {
+		t.Error("error coord hor")
+	}
+	coord = searchVertical(board, gomoku.BoardCellFoe)
 	if coord.size != 2 || coord.x != 3 || coord.y != 2 {
 		t.Error("error coord hor")
 	}
@@ -71,12 +112,30 @@ func TestSearchHorizontalSingle(t *testing.T) {
 	board.Cells[3][1] = gomoku.BoardCellFoe
 	board.Cells[3][3] = gomoku.BoardCellOwn
 
-	coord = searchHorizontal(&board, gomoku.BoardCellOwn)
+	coord = searchHorizontal(board, gomoku.BoardCellOwn)
 	if coord.size != 1 || coord.x != 5 || coord.y != 0 {
 		t.Error("error coord hor")
 	}
-	coord = searchHorizontal(&board, gomoku.BoardCellFoe)
+	coord = searchHorizontal(board, gomoku.BoardCellFoe)
 	if coord.size != 1 || coord.x != 4 || coord.y != 1 {
+		t.Error("error coord hor")
+	}
+}
+
+func TestSearchHorizontalZero(t *testing.T) {
+	var board gomoku.Board
+	var coord searchResult
+	err := board.Init(10)
+	if err != nil {
+		t.Error("error was supposed to be nil")
+	}
+
+	coord = searchHorizontal(board, gomoku.BoardCellOwn)
+	if coord.size != 0 {
+		t.Error("error coord hor")
+	}
+	coord = searchHorizontal(board, gomoku.BoardCellFoe)
+	if coord.size != 0 {
 		t.Error("error coord hor")
 	}
 }
@@ -95,12 +154,30 @@ func TestSearchVerticalSingle(t *testing.T) {
 	board.Cells[3][1] = gomoku.BoardCellFoe
 	board.Cells[3][3] = gomoku.BoardCellOwn
 
-	coord = searchVertical(&board, gomoku.BoardCellOwn)
+	coord = searchVertical(board, gomoku.BoardCellOwn)
 	if coord.size != 1 || coord.x != 1 || coord.y != 1 {
 		t.Error("error coord hor")
 	}
-	coord = searchVertical(&board, gomoku.BoardCellFoe)
+	coord = searchVertical(board, gomoku.BoardCellFoe)
 	if coord.size != 1 || coord.x != 1 || coord.y != 3 {
+		t.Error("error coord hor")
+	}
+}
+
+func TestSearchVerticalZero(t *testing.T) {
+	var board gomoku.Board
+	var coord searchResult
+	err := board.Init(10)
+	if err != nil {
+		t.Error("error was supposed to be nil")
+	}
+
+	coord = searchVertical(board, gomoku.BoardCellOwn)
+	if coord.size != 0 {
+		t.Error("error coord hor")
+	}
+	coord = searchVertical(board, gomoku.BoardCellFoe)
+	if coord.size != 0 {
 		t.Error("error coord hor")
 	}
 }
