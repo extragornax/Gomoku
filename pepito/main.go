e630a854a05623f052d59da95fa09bfc8ae242fe
Anthony <anthony.tram@epitech.eu>
2018-10-26 23:10:13 +0200

    added diagonal up, check for diagonl up/down is bugged

--- pepito/main.go
+++ pepito/main.go
@@ -11,7 +11,7 @@ func kappa(gmk gomoku.Gomoku, r *rand.Rand) {
 	var b uint = uint(r.Intn(19))
 	gmk.Play(a, b)
 }
-func check_diagonal_up(game gomoku.Gomoku, x uint, y uint, who uint8) uint {
+func check_diagonal_up(game gomoku.Gomoku, x uint, y uint, infos *searchResult, who uint8) uint {
 	var count uint = 0
 	var j uint = x
 	for i := int8(y); i < int8(game.Board.Size); i-- {
@@ -19,38 +19,56 @@ func check_diagonal_up(game gomoku.Gomoku, x uint, y uint, who uint8) uint {
 			return count
 		}
 		if game.Board.Cells[i][j] == who {
-			fmt.Printf("coucou = %d\n", count)
 			count += 1
 		}
 		j += 1
 	}
+	if count > infos.size {
+		infos.size = count
+		infos.x = x
+		infos.y = y
+	}
 	return count
 }
 
-func find_diag_up(game gomoku.Gomoku, who uint8) {
-	var max_y int = -1
-	var max_x int = -1
-	var max_count uint = 0
-	for i := uint(0); i < game.Board.Size; i++ {
-		for j := uint(0); j < game.Board.Size; j++ {
-			if game.Board.Cells[i][j] == who {
-				tmp_count := check_diagonal_up(game, j, i, who)
-				fmt.Printf("%d\n", tmp_count)
-				if tmp_count > max_count {
-					max_count = tmp_count
-					max_x = int(j)
-					max_y = int(i)
-					fmt.Printf("DIAGONAL UP: %d %d %d\n", max_x, max_y, max_count)
-				}
-			}
-		}
+func check_empty_before_after_up(game gomoku.Gomoku, who uint8, infos *searchResult) {
+	if infos.x+1 < game.Board.Size && infos.y >= 0 &&
+		game.Board.Cells[infos.x+1][infos.y-1] == who {
+		infos.blockedAfter = false
+	} else {
+		infos.blockedAfter = true
+	}
+	if infos.x-1 >= 0 && infos.y+1 < game.Board.Size &&
+		game.Board.Cells[infos.x-1][infos.y+1] == who {
+		infos.blockedBefore = false
+	} else {
+		infos.blockedBefore = true
 	}
 }
 
-func check_diagonal_down(game gomoku.Gomoku, x uint, y uint, who uint8) uint {
+func find_diag_up(game gomoku.Gomoku, who uint8) searchResult {
+	var infos searchResult
+	infos.resultType = searchDiagUpResult
+	infos.x = 0
+	infos.y = 0
+	for i := uint(0); i < game.Board.Size; i++ {
+		for j := uint(0); j < game.Board.Size; j++ {
+			if game.Board.Cells[i][j] == who {
+				check_diagonal_up(game, j, i, &infos, who)
+			}
+		}
+	}
+	if infos.size > 0 {
+		check_empty_before_after_up(game, who, &infos)
+	}
+	fmt.Printf("DIAGONAL UP: %d %d %d %d\n", infos.x, infos.y, infos.size, infos.blockedBefore)
+	return infos
+}
+
+func check_diagonal_down(game gomoku.Gomoku, x uint, y uint, infos *searchResult, who uint8) uint {
 	var count uint = 0
-	var j uint = x
-	for i := int8(y); i < int8(game.Board.Size); i++ {
+	var j uint = infos.x
+	for i := infos.y; i < game.Board.Size; i++ {
 		if i < 0 || j < 0 || j >= (game.Board.Size) {
 			return count
 		}
@@ -59,46 +77,47 @@ func check_diagonal_down(game gomoku.Gomoku, x uint, y uint, who uint8) uint {
 		}
 		j += 1
 	}
+	if infos.size < count {
+		infos.x = x
+		infos.y = y
+		infos.size = count
+	}
 	return count
 }
 
-func find_diag_down(game gomoku.Gomoku, who uint8) {
-	var max_y int = -1
-	var max_x int = -1
-	var max_count uint = 0
-	for i := uint(0); i < game.Board.Size; i++ {
-		for j := uint(0); j < game.Board.Size; j++ {
-			if game.Board.Cells[i][j] == who {
-				tmp_count := check_diagonal_down(game, j, i, who)
-				if tmp_count > max_count {
-					max_count = tmp_count
-					max_x = int(j)
-					max_y = int(i)
-					fmt.Printf("DIAGONAL DOWN: %d %d %d\n", max_x, max_y, max_count)
-				}
+func check_empty_before_after(game gomoku.Gomoku, who uint8, infos *searchResult) {
+	if infos.x+1 < game.Board.Size && infos.y+1 < game.Board.Size &&
+		game.Board.Cells[infos.x+1][infos.y+1] != gomoku.BoardCellEmpty {
+		infos.blockedAfter = true
+	} else {
+		infos.blockedAfter = false
+	}
+	if infos.x-1 >= 0 && infos.y-1 >= 0 &&
+		game.Board.Cells[infos.x-1][infos.y-1] != gomoku.BoardCellEmpty {
+		infos.blockedBefore = true
+	} else {
+		infos.blockedBefore = false
+	}
+}
+
+func find_diag_down(game gomoku.Gomoku, who uint8) searchResult {
+	var infos searchResult
+	infos.resultType = searchDiagDownResult
+	infos.x = 0
+	infos.y = 0
+	for y := uint(0); y < game.Board.Size; y++ {
+		for x := uint(0); x < game.Board.Size; x++ {
+			if game.Board.Cells[y][x] == who {
+				check_diagonal_down(game, x, y, &infos, who)
 			}
 		}
 	}
-}
-
-func main() {
-	var game gomoku.Gomoku
-	game.Init()
-	r := rand.New(rand.NewSource(99))
-	game.Run()
-	for game.Live {
-		kappa(game, r)
-		find_diag_up(game, gomoku.BoardCellFoe)
-		find_diag_down(game, gomoku.BoardCellFoe)
-		game.Run()
+	if infos.size > 0 {
+		check_empty_before_after(game, who, &infos)
 	}
+	fmt.Printf("DIAGONAL DOWN: %d %d %d %d\n", infos.x, infos.y, infos.size, infos.blockedBefore)
+	return infos
 }
-
-package main
-
-import (
-	"gomoku/gomoku"
-)
 
 type uvector struct {
 	x uint
@@ -108,10 +127,16 @@ type uvector struct {
 func main() {
 	var game gomoku.Gomoku
 	game.Init()
+	// r := rand.New(rand.NewSource(99))
 	game.Run()
+	var i uint = 6
+	var j uint = 6
 	for game.Live {
-		trial := decisionTake(&game)
-		game.Play(trial.x, trial.y)
+		j += 1
+		i -= 1
+		game.Play(i, j)
+		// find_diag_up(game, gomoku.BoardCellFoe)
+		find_diag_down(game, gomoku.BoardCellFoe)
 		game.Run()
 	}
 }
